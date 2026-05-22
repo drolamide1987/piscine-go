a490ad8fb5f4ee00b010a23d13a3ec158576cb6f
drolamide1987 <developerchiamaka@gmail.com>
2026-05-22 05:38:37 +0100

    done

--- retainfirsthalf.go
+++ retainfirsthalf.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+)
+
+func RetainFirstHalf(str string) string {
+	
+
+	if len(str) <= 1{
+		return str
+	}
+
+	half := len(str) / 2 
+	return str[:half]
+}
+
+func main() {
+	fmt.Println(RetainFirstHalf("This is the 1st halfThis is the 2nd half"))
+	fmt.Println(RetainFirstHalf("A"))
+	fmt.Println(RetainFirstHalf(""))
+	fmt.Println(RetainFirstHalf("Hello World"))
+}
